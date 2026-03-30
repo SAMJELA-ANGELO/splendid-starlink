81b44011ec73f4fbc36c30cfb61fcad5e738e349
TATA Ronaldo <tronaldowiysanyuy@gmail.com>
2026-03-30 08:35:00 +0100

    New changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { PaymentsModule } from '../payments/payments.module';
+import { MikrotikModule } from '../mikrotik/mikrotik.module';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
@@ -13,6 +14,7 @@ import { LocalStrategy } from './local.strategy';
   imports: [
     UsersModule,
     PaymentsModule,
+    MikrotikModule,
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
